43686d54f48c0ed06fe1586419cec5e62ee52c4c
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-02-10 00:36:19 +0900

    Create scatter.py

---
+++ scatter.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from pandas_datareader import data as pdr
+import yfinance as yf
+import matplotlib.pyplot as plt
+yf.pdr_override()
+
+dow = pdr.get_data_yahoo('^DJI', '2000-01-04')
+kospi = pdr.get_data_yahoo('^KS11', '2000-01-04')
+df = pd.DataFrame({'DOW': dow['Close'], 'KOSPI': kospi['Close']})
+df = df.fillna(method='bfill')
+df = df.fillna(method='ffill')
+
+plt.figure(figsize=(7, 7))
+plt.scatter(df['DOW'], df['KOSPI'], marker = '.')
+plt.xlabel('Dow Jones Industrial Average')
+plt.ylabel('KOSPI')
+plt.show()
